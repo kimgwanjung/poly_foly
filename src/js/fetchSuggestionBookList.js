033376de3cc4c5434b0e651f677203e337dec0d5
kimgwanjung <qwe2846@naver.com>
2023-05-28 20:11:07 +0900

    json

--- src/js/fetchSuggestionBookList.js
+++ src/js/fetchSuggestionBookList.js
@@ -1,5 +1,5 @@
 function fetchSuggestionBookList() {
-  fetch('_data/suggestionBook.json')
+  fetch('/_data/suggestionBook.json')
     .then((response) => response.json())
     .then((data) => {
       const bookListWrapper = document.querySelector(
