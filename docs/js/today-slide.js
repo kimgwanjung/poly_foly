1c290a2ba6635210e620413e3fe580bbdbc52d31
kimgwanjung <qwe2846@naver.com>
2023-05-29 00:53:48 +0900

    logo 추가

--- docs/js/today-slide.js
+++ docs/js/today-slide.js
@@ -1,27 +1,26 @@
 (function () {
-    const slideList = document.querySelector('.slide_list');  // Slide parent dom
-    const slideContents = document.querySelectorAll('.slide_content');  // each slide dom
-    const slideBtnNext = document.querySelector('.slide_btn_next'); // next button
-    const slideBtnPrev = document.querySelector('.slide_btn_prev'); // prev button
+    const slideList = document.querySelector('.slide_list');  
+    const slideContents = document.querySelectorAll('.slide_content');  
+    const slideBtnNext = document.querySelector('.slide_btn_next'); 
+    const slideBtnPrev = document.querySelector('.slide_btn_prev'); 
     const pagination = document.querySelector('.slide_pagination');
-    const slideLen = slideContents.length;  // slide length
-    const slideWidth = 400; // slide width
-    const slideSpeed = 300; // slide speed
-    const startNum = 0; // initial slide index (0 ~ 4)
+    const slideLen = slideContents.length;  
+    const slideWidth = 400; 
+    const slideSpeed = 300; 
+    const startNum = 0; 
     
     slideList.style.width = slideWidth * (slideLen + 2) + "px";
     
-    // Copy first and last slide
+
     let firstChild = slideList.firstElementChild;
     let lastChild = slideList.lastElementChild;
     let clonedFirst = firstChild.cloneNode(true);
     let clonedLast = lastChild.cloneNode(true);
 
-    // Add copied Slides
     slideList.appendChild(clonedFirst);
     slideList.insertBefore(clonedLast, slideList.firstElementChild);
 
-    // Add pagination dynamically
+
     let pageChild = '';
     for (var i = 0; i < slideLen; i++) {
         pageChild += '<li class="dot';
@@ -29,15 +28,14 @@
         pageChild += '" data-index="' + i + '"><a href="#"></a></li>';
     }
     pagination.innerHTML = pageChild;
-    const pageDots = document.querySelectorAll('.dot'); // each dot from pagination
+    const pageDots = document.querySelectorAll('.dot'); 
 
     slideList.style.transform = "translate3d(-" + (slideWidth * (startNum + 1)) + "px, 0px, 0px)";
 
-    let curIndex = startNum; // current slide index (except copied slide)
-    let curSlide = slideContents[curIndex]; // current slide dom
+    let curIndex = startNum; 
+    let curSlide = slideContents[curIndex]; 
     curSlide.classList.add('slide_active');
 
-  /** Next Button Event */
     slideBtnNext.addEventListener('click', function() {
         if (curIndex <= slideLen - 1) {
         slideList.style.transition = slideSpeed + "ms";
@@ -57,7 +55,6 @@
         pageDots[curIndex].classList.add('dot_active');
     });
 
-    /** Prev Button Event */
     slideBtnPrev.addEventListener('click', function() {
         if (curIndex >= 0) {
         slideList.style.transition = slideSpeed + "ms";
@@ -77,7 +74,6 @@
         pageDots[curIndex].classList.add('dot_active');
     });
 
-  /** Pagination Button Event */
     let curDot;
     Array.prototype.forEach.call(pageDots, function (dot, i) {
         dot.addEventListener('click', function (e) {
