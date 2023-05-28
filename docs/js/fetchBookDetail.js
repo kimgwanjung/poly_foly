10067a28b7e03d70a7a8bfda5008991c2be76977
kimgwanjung <qwe2846@naver.com>
2023-05-28 17:39:52 +0900

    상세페이지

---
+++ docs/js/fetchBookDetail.js
@@ -0,0 +1,33 @@
+function fetchBookDetail(id) {
+  fetch(`/_data/bookDetail_${id}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data, "d");
+      const bookData = data.data[0];
+
+      const imgWrapper = document.querySelector(".img-wrapper");
+      const bookImg = imgWrapper.querySelector(".book-imgs");
+      bookImg.src = bookData.image;
+
+      const titleElement = document.querySelector(".title");
+      titleElement.innerText = bookData.title;
+
+      const pricePercentElement = document.querySelector(".price-percent");
+      pricePercentElement.innerText = bookData.percent;
+
+      const priceElement = document.querySelector(".price");
+      priceElement.innerText = bookData.price;
+
+      const pointElement = document.querySelector(".point");
+      pointElement.innerText = bookData.point;
+
+      const startDateElement = document.querySelector(".start-date");
+      startDateElement.innerText = bookData.startDate;
+
+      const addressElement = document.querySelector(".address");
+      addressElement.innerText = bookData.address;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
---
+++ src/js/fetchSuggestionBookList.js
@@ -0,0 +1,49 @@
+function fetchSuggestionBookList() {
+  fetch("/_data/suggestionBook.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const bookListWrapper = document.querySelector(
+        ".book-list-suggestion-wrapper"
+      );
+
+      data.data.forEach((item) => {
+        const bookItemWrapper = document.createElement("div");
+        bookItemWrapper.classList.add("book-item-wrapper");
+
+        const bookImg = document.createElement("img");
+        bookImg.classList.add("book-img");
+        bookImg.src = item.image;
+
+        const bookTitle = document.createElement("div");
+        bookTitle.classList.add("book-title");
+        bookTitle.innerText = item.title;
+
+        const author = document.createElement("div");
+        author.classList.add("author");
+        author.innerText = item.author;
+
+        const price = document.createElement("div");
+        price.classList.add("price");
+
+        const priceBold = document.createElement("span");
+        priceBold.classList.add("price-bold");
+        priceBold.innerText = item.price;
+
+        const priceText = document.createElement("span");
+        priceText.innerText = "원";
+
+        price.appendChild(priceBold);
+        price.appendChild(priceText);
+
+        bookItemWrapper.appendChild(bookImg);
+        bookItemWrapper.appendChild(bookTitle);
+        bookItemWrapper.appendChild(author);
+        bookItemWrapper.appendChild(price);
+
+        bookListWrapper.appendChild(bookItemWrapper);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
