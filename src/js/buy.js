e80db3fc68de5e1e3eaf36e900828fd2c3321b26
kimgwanjung <qwe2846@naver.com>
2023-05-31 22:28:59 +0900

    1. 레이아웃 중첩 2. flex 중첩 3.

--- src/js/buy.js
+++ src/js/buy.js
@@ -1,6 +1,5 @@
 const id = getParameterByName('id');
     fetchBookDetail(id);
-
     // Add click event listener to the "구입하기" button
     const buyButton = document.querySelector('.buy-button');
     buyButton.addEventListener('click', function() {
--- src/js/fetchTodayBookList.js
+++ src/js/fetchTodayBookList.js
@@ -3,47 +3,36 @@ function fetchTodayBookList() {
     .then((response) => response.json())
     .then((data) => {
       const bookListWrapper = document.querySelector(".book-list-wrapper");
-
       data.data.forEach((item) => {
         const bookItemWrapper = document.createElement("div");
         bookItemWrapper.classList.add("book-item-wrapper");
-
         const bookImg = document.createElement("img");
         bookImg.classList.add("book-img");
         bookImg.src = item.image;
-
         const bookTitle = document.createElement("div");
         bookTitle.classList.add("book-title");
         bookTitle.innerText = item.title;
-
         const author = document.createElement("div");
         author.classList.add("author");
         author.innerText = item.author;
-
         const price = document.createElement("div");
         price.classList.add("price");
-
         const priceBold = document.createElement("span");
         priceBold.classList.add("price-bold");
         priceBold.innerText = item.price;
-
         const priceText = document.createElement("span");
         priceText.innerText = "원";
-
         price.appendChild(priceBold);
         price.appendChild(priceText);
-
         bookItemWrapper.appendChild(bookImg);
         bookItemWrapper.appendChild(bookTitle);
         bookItemWrapper.appendChild(author);
         bookItemWrapper.appendChild(price);
-
         // 클릭 이벤트 추가
         bookItemWrapper.addEventListener("click", () => {
           // 상세 페이지로 이동
           window.location.href = `bookdetail.html?id=${item.id}`;
         });
-
         bookListWrapper.appendChild(bookItemWrapper);
       });
     })
